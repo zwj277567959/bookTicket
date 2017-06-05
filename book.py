f2b28db1ff7d359403f7c67dda7a018a4a9b6f2a
zhaowenjun <277567959@qq.com>
2017-06-05 19:07:24 +0800

    book.py接口精确化

--- book.py
+++ book.py
@@ -18,14 +18,13 @@ def get_item(html):
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find('div', attrs={'id': 'jxContentPanel'})
     for tmp in table.find_all('div', attrs={'class': 'b_result_box js_list_block b_result_commentbox'}):
-        txt = ''
-        name = tmp.find('a', attrs={'class': 'e_title js_list_name'}).get_text().strip()
+        txt = {}
+        txt["name"] = tmp.find('a', attrs={'class': 'e_title js_list_name'}).get_text().strip()
         if tmp.find('p', attrs={'class': 'item_price js_hasprice'}) is not None:
-            price = tmp.find('p', attrs={'class': 'item_price js_hasprice'}).get_text().strip()
+            txt["price"] = tmp.find('p', attrs={'class': 'item_price js_hasprice'}).get_text().strip()
         else:
-            price = tmp.find('div', attrs={'class': 'hotel_price'}).get_text().strip()
-        locate = tmp.find('span', attrs={'class': 'area_contair'}).get_text().strip()
-        txt += name + ' ' + price + ' ' + locate
+            txt["price"] = tmp.find('div', attrs={'class': 'hotel_price'}).get_text().strip()
+        txt["locate"] = tmp.find('span', attrs={'class': 'area_contair'}).get_text().strip()
         txt1.append(txt)
     return txt1
 
@@ -58,11 +57,11 @@ def bookhotel():
         driver.find_element_by_class_name('next').click()
         driver.close()
         display.stop()
-        return jsonify({'txt': txt})
+        return jsonify({'status': 200, 'data': txt})
     except Exception, e:
         driver.close()
         display.stop()
-        return jsonify({'txt': e})
+        return jsonify({'status': -1, 'error': e})
 
 @app.route('/bookFlight', methods=['GET'])
 def bookflight():
@@ -98,20 +97,24 @@ def bookflight():
         sleep(2)
         list = []
         for tmp in driver.find_elements_by_class_name("search_box"):
-            txt = ''
-            txt += tmp.find_element_by_class_name('logo').text.replace("\n", " ") + " " \
-                   + tmp.find_element_by_class_name('right').text.replace("\n", "(") + ")" + "-" \
-                   + tmp.find_element_by_class_name('left').text.replace("\n", "(") + ")" + " " \
-                   + tmp.find_element_by_class_name('service').text.replace("\n", " ") + " " \
-                   + tmp.find_element_by_class_name('price').text.replace("\n", " ")
+            txt = {}
+            txt["company"] = tmp.find_element_by_class_name('logo').text.replace("\n", " ")
+            right = tmp.find_element_by_class_name('right').text.split("\n")
+            txt["startime"] = right[0]
+            txt["fromPlace"] = right[1]
+            left = tmp.find_element_by_class_name('left').text.replace("\n")
+            txt["endtime"] = left[0]
+            txt["toPlace"] = left[1]
+            txt["unDelayRate"] = tmp.find_element_by_class_name('service').text.replace("\n", " ")
+            txt["price"] = tmp.find_element_by_class_name('price').text.replace("\n", " ")
             list.append(txt)
         driver.close()
         display.stop()
-        return jsonify({'txt': list})
+        return jsonify({'status': 200, 'data': list})
     except Exception, e:
         driver.close()
         display.stop()
-        return jsonify({'txt': e})
+        return jsonify({'status': -1, 'error': e})
 
 @app.route('/bookTrain', methods=['GET'])
 def booktrain():
@@ -120,8 +123,8 @@ def booktrain():
     fromDate = request.args.get('fromDate')
 
     try:
-        display = Display(visible=0, size=(1024, 768))
-        display.start()
+        # display = Display(visible=0, size=(1024, 768))
+        # display.start()
         driver = webdriver.Chrome(chrome_options=chrome_options)
         driver.get('http://train.qunar.com/')
         sleep(2)
@@ -138,23 +141,31 @@ def booktrain():
         sleep(2)
         trainllist = []
         for tmp in driver.find_elements_by_class_name("js_listinfo"):
-            txt = ''
-            txt += tmp.find_element_by_class_name("col1").text.replace("\n", " ") + " " \
-                   + tmp.find_element_by_class_name("col2").text.replace("\n", " ") + " " \
-                   + tmp.find_element_by_class_name("startime").text.replace("\n", " ") + " " \
-                   + tmp.find_element_by_class_name("endtime").text.replace("\n", " ") + " " \
-                   + tmp.find_element_by_class_name("col6").text.replace("\n", " ") + " " \
-                   + tmp.find_element_by_class_name("col3").text.replace("\n", " ") + " " \
-                   + tmp.find_element_by_class_name("col4").text.replace("\n", " ")
+            txt = {}
+            txt["train"] = tmp.find_element_by_class_name("train").text.replace("\n", " ")
+            txt["fromPlace"] = tmp.find_element_by_class_name("start").text.replace("\n", " ")
+            txt["toPlace"] = tmp.find_element_by_class_name("end").text.replace("\n", " ")
+            txt["startime"] = tmp.find_element_by_class_name("startime").text.replace("\n", " ")
+            txt["endtime"] = tmp.find_element_by_class_name("endtime").text.replace("\n", " ")
+            txt["timelong"] = tmp.find_element_by_class_name("col6").text.replace("\n", " ")
+            ticketType = tmp.find_element_by_class_name("col3").text.split("\n")
+            ticketNum = tmp.find_element_by_class_name("col4").text.split("\n")
+            txt["tickets"] =[]
+            for i in range(len(ticketNum)):
+                ticket ={}
+                ticket["type"] = ticketType[i].split(" ")[0]
+                ticket["price"] = ticketType[i].split(" ")[1]
+                ticket["num"] = ticketNum[i]
+                txt["tickets"].append(ticket)
             trainllist.append(txt)
-
         driver.close()
-        display.stop()
-        return jsonify({'txt': trainllist})
+        # display.stop()
+
+        return jsonify({'status': 200, 'data': trainllist})
     except Exception, e:
         driver.close()
-        display.stop()
-        return jsonify({'txt': e})
+        # display.stop()
+        return jsonify({'status': -1, 'error': e})
 
 @app.route('/')
 def index():
